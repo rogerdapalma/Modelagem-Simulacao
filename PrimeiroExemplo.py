4869b47a5430c379aa74a97fe90f439a7b75d7b8
Roger <94903084+rogerdapalma@users.noreply.github.com>
2024-08-06 10:35:00 -0300

    Add files via upload

---
+++ PrimeiroExemplo.py
@@ -0,0 +1,22 @@
+from vpython import *
+#Web VPython 3.2
+#X
+#Y 
+#Z 
+esfera1 = sphere(pos=vector(0,0,0),radius=1,color=color.red)
+
+esfera2 = sphere(pos=vector(10,0,0),radius=1,color=color.blue)
+
+esfera3 = sphere(pos=vector(0,10,0),radius=1,color=color.orange)
+
+esfera4 = sphere(pos=vector(10,10,0),radius=1,color=color.white)
+
+esfera5 = sphere(pos=vector(0,0,10),radius=1,color=color.red)
+
+esfera6 = sphere(pos=vector(10,0,10),radius=1,color=color.blue)
+
+esfera7 = sphere(pos=vector(0,10,10),radius=1,color=color.orange)
+
+esfera8 = sphere(pos=vector(10,10,10),radius=1,color=color.white)
+
+box(pos=vec(5, 5, 5),length=10, height=10, width=10, color=color.cyan)
