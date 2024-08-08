f5ddff0f56a9e76f0116599e436ad9c5661b209e
Roger <94903084+rogerdapalma@users.noreply.github.com>
2024-08-08 08:10:18 -0300

    Update SegundoExemplo.py

--- SegundoExemplo.py
+++ SegundoExemplo.py
@@ -1,3 +1,4 @@
+'''
 from vpython import *
 #Web VPython 3.2
 esquerda = box(pos=vec(-10, 0, 0),length=1, height=1, width=1, color=color.blue)
@@ -14,3 +15,27 @@ while True:
     else:
         break  # Para o movimento quando a esfera alcançar o limite
 # tafera para proxima aula, fazer que a esfera bata no quadrado direito e va para o quadrado esquerdo
+'''
+
+Web VPython 3.2
+from vpython import *
+
+# Criação dos objetos
+esquerda = box(pos=vec(-10, 0, 0), length=1, height=1, width=1, color=color.blue)
+direita = box(pos=vec(10, 0, 0), length=1, height=1, width=1, color=color.red)
+esfera = sphere(pos=vector(0, 0, 0), radius=1, color=color.orange)
+
+def mover_direita():
+    while esfera.pos.x < direita.pos.x - 2:
+        rate(60)
+        esfera.pos.x += 1
+
+def mover_esquerda():
+    while esfera.pos.x > esquerda.pos.x + 2:
+        rate(60)
+        esfera.pos.x -= 1
+
+# Loop principal
+while True:
+    mover_direita()
+    mover_esquerda()
